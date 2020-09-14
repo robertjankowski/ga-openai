b2033b51fce6de4535b27b600fa3c7ead5419615
robertjankowski <robjanw11@wp.pl>
2020-09-14 12:17:15 -0700

    add trained model

--- scripts/tests/bipedalwalker/testing_model_bipedalwalker_new.py
+++ scripts/tests/bipedalwalker/testing_model_bipedalwalker_new.py
@@ -21,15 +21,15 @@ def test_mlp_torch(model, input_size: int, is_reduced=False):
 if __name__ == '__main__':
     env = gym.make('BipedalWalker-v2')
     # env = gym.wrappers.Monitor(env, 'bipedalwalker', video_callable=lambda episode_id: True, force=True)
-    env.seed(123)
+    env.seed(102)
 
     INPUT_SIZE = 24
-    HIDDEN_SIZE = [10, 12]
+    HIDDEN_SIZE = [20, 12, 12]
     OUTPUT_SIZE = 4
 
     model = DeepMLPTorch(INPUT_SIZE, OUTPUT_SIZE, *HIDDEN_SIZE)
     model.load(
-        "../../../models/bipedalwalker/large_model/model-layers=24-[10, 12]-4-09-13-2020_13-23_NN=DeepBipedalWalkerIndividual_POPSIZE=10_GEN=10_PMUTATION_0.01_PCROSSOVER_0.8_I=10_SCORE=1.1157546929189408.npy"
+        "../../../models/bipedalwalker/large_model/model-layers=24-[20, 12, 12]-4-09-14-2020_11-54_NN=DeepBipedalWalkerIndividual_POPSIZE=40_GEN=5000_PMUTATION_0.1_PCROSSOVER_0.85_I=4949_SCORE=53.64846821325179.npy"
     )
     test_mlp_torch(model, input_size=INPUT_SIZE, is_reduced=True)
     env.close()
