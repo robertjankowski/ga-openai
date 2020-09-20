7127abf972a2401e83fbe965a532851a13b53344
robertjankowski <robjanw11@wp.pl>
2020-09-20 10:17:13 -0700

    evaluate small bipedalwalker model

--- scripts/bipedalwalker/offline_learning/train_small_bipedalwalker.py
+++ scripts/bipedalwalker/offline_learning/train_small_bipedalwalker.py
@@ -5,22 +5,27 @@ import torch.nn as nn
 
 from nn.mlp import DeepMLPTorch
 
+INPUT_SHAPE = 8
+OUTPUT_SHAPE = 4
+HIDDEN_SIZES = [8]
 
-def load_dataset(features_path: str, labels_path: str):
-    X = pd.read_csv(features_path, header=None)
+
+def load_dataset(features_path: str, labels_path: str, use_columns=None):
+    X = pd.read_csv(features_path, header=None, usecols=use_columns)
     y = pd.read_csv(labels_path, header=None)
     X = torch.tensor(X.values, dtype=torch.float)
     y = torch.tensor(y.values, dtype=torch.float)
     return X, y
 
 
-def create_model(input_size, output_size, *hidden_size):
+def create_model(input_size, output_size, hidden_size):
     model = DeepMLPTorch(input_size, output_size, hidden_size)
     opt = torch.optim.Adam(model.parameters(), lr=1e-3)
     return model, opt
 
 
 def train(train_data, model, opt, loss_func, epochs):
+    min_loss = 999999
     for epoch in range(epochs):
         for xb, yb in train_data:
             pred = model(xb)
@@ -28,26 +33,28 @@ def train(train_data, model, opt, loss_func, epochs):
             loss.backward()
             opt.step()
             opt.zero_grad()
-        print('Epoch {}: loss = {}'.format(epoch, loss_func(student_model(xb), yb)))
+        new_min_loss = loss_func(student_model(xb), yb)
+        if new_min_loss < min_loss:
+            min_loss = new_min_loss
+            torch.save(student_model,
+                       "model-layers={}-[{}]-{}-NN=StudentModel-EPOCH={}_LOSS={}.pt".format(
+                           INPUT_SHAPE, ','.join(map(str, HIDDEN_SIZES)), OUTPUT_SHAPE, epochs, min_loss))
+        print('Epoch {}: loss = {}'.format(epoch, min_loss))
 
 
 if __name__ == '__main__':
     X, y = load_dataset(
-        "../../../models/bipedalwalker/generated_data/features_data_model-layers=24-[20, 12, 12]-4-09-14-2020_11-54_NN=DeepBipedalWalkerIndividual_POPSIZE=40_GEN=5000_PMUTATION_0_NRUNS=10000.csv",
-        "../../../models/bipedalwalker/generated_data/labels_data_model-layers=24-[20, 12, 12]-4-09-14-2020_11-54_NN=DeepBipedalWalkerIndividual_POPSIZE=40_GEN=5000_PMUTATION_0_NRUNS=10000.csv")
+        "features_data_model-layers=24-[20, 12, 12]-4-09-14-2020_11-54_NN=DeepBipedalWalkerIndividual_POPSIZE=40_GEN=5000_PMUTATION_0_NRUNS=10000_subset.csv",
+        "labels_data_model-layers=24-[20, 12, 12]-4-09-14-2020_11-54_NN=DeepBipedalWalkerIndividual_POPSIZE=40_GEN=5000_PMUTATION_0_NRUNS=10000_subset.csv",
+        use_columns=list(range(INPUT_SHAPE))
+    )
 
-    BATCH_SIZE = 32
-    EPOCHS = 10
+    BATCH_SIZE = 64
+    EPOCHS = 50
     train_ds = TensorDataset(X, y)
     train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
 
-    INPUT_SHAPE = 24
-    OUTPUT_SHAPE = 4
-    HIDDEN_SIZES = [2]
-    student_model, opt = create_model(INPUT_SHAPE, OUTPUT_SHAPE, HIDDEN_SIZES)
+    student_model, opt = create_model(INPUT_SHAPE, OUTPUT_SHAPE, *HIDDEN_SIZES)
     loss_func = nn.modules.loss.MSELoss(size_average=False)
 
     train(train_dl, student_model, opt, loss_func, EPOCHS)
-    torch.save(student_model,
-               "../../../models/bipedalwalker/large_model/model-layers={}-[{}]-{}-NN=StudentModel.pt".format(
-                   INPUT_SHAPE, ','.join(map(str, HIDDEN_SIZES)), OUTPUT_SHAPE))
