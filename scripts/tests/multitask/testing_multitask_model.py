9ff2e122ea42f1ee776bb43af5e5bab6b7124246
robertjankowski <robjanw11@wp.pl>
2020-09-26 13:25:53 -0700

    testing multitask model

--- scripts/tests/multitask/testing_multitask_model.py
+++ scripts/tests/multitask/testing_multitask_model.py
@@ -1,32 +1,44 @@
 import gym
 import tensorflow as tf
+import numpy as np
+
 from scripts.multitask_learning.multitask_learning import MultiTaskModel, Shape
 
 from gym import wrappers
 
 
-# TODO
-# 1. model should have two inputs - play games simultaneously?
-# 2. output also should be converted to right format
+def test_model(env_bipedalwalker, env_cartpole, model, bipedalwalker_shape, cartpole_shape):
+    obs_bipedalwalker = env_bipedalwalker.reset()
+    obs_cartpole = env_cartpole.reset()
+    while True:
+        env_bipedalwalker.render()
+        env_cartpole.render()
 
+        action = model({
+            "first_input": np.reshape(obs_cartpole, (1, cartpole_shape.input)),
+            # "first_input": np.reshape(np.random.rand(cartpole_shape.input), (1, cartpole_shape.input)),
+            "second_input": np.reshape(obs_bipedalwalker, (1, bipedalwalker_shape.input))
+        })
+        action_cartpole, action_bipedalwalker = action
+        action_cartpole = round(action_cartpole.numpy()[0].item())
+        if action_cartpole > 1:
+            action_cartpole = 1
+        action_bipedalwalker = action_bipedalwalker.numpy()[0]
 
-def test_mlp_torch(model, input_size: int, is_reduced=False):
-    obs = env.reset()
-    for _ in range(2000):
-        env.render()
-        if is_reduced:
-            obs = obs[:input_size]
-        action = model(obs)
-        action = action.detach().numpy()
-        obs, reward, done, _ = env.step(action)
-        if done:
+        obs_bipedalwalker, _, done_bipedalwalker, _ = env_bipedalwalker.step(action_bipedalwalker)
+        obs_cartpole, _, done_cartpole, _ = env_cartpole.step(action_cartpole)
+
+        if done_bipedalwalker:
             break
 
 
 if __name__ == '__main__':
-    env = gym.make('BipedalWalker-v2')
+    env_bipedalwalker = gym.make('BipedalWalker-v2')
+    env_cartpole = gym.make("CartPole-v0")
     # env = gym.wrappers.Monitor(env, 'bipedalwalker_small_4_4_4', video_callable=lambda episode_id: True, force=True)
-    env.seed(312)
+
+    env_bipedalwalker.seed(312)
+    env_cartpole.seed(312)
 
     cartpole_shape = Shape(4, 1)
     bipedalwalker_shape = Shape(24, 4)
@@ -35,6 +47,7 @@ if __name__ == '__main__':
     model_path = tf.train.latest_checkpoint("../../multitask_learning/multitask-model-test")
     model.load_weights(model_path)
 
-    test_mlp_torch(model.model, input_size=bipedalwalker_shape.input, is_reduced=True)
+    test_model(env_bipedalwalker, env_cartpole, model.model, bipedalwalker_shape, cartpole_shape)
 
-    env.close()
+    env_bipedalwalker.close()
+    env_cartpole.close()
